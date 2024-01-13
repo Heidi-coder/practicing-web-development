4a9d23d954f08bf954f78b746e33ed23571f3dab
Heidi Gibby <heidigibby@gmail.com>
2024-01-13 14:13:19 -0700

    Get feedback section submitting feedback to console

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let feedbackText;
-let feedbackEmail;
+document.addEventListener('DOMContentLoaded', function() {
 
-document.getElementById("submitButton").addEventListener("click", function (){
-  feedbackText = document.getElementById("feedbackTextId").value;
-  console.log(feedbackText);
+  let feedbackText = document.getElementById("feedbackTextId").value;
+  let emailText = document.getElementById("feedbackEmailId").value;
+
+  document.getElementById("submitButton").addEventListener("click", clickSubmitButton);
+
+  function retrieveFeedback(){
+    feedbackText = document.getElementById("feedbackTextId").value;
+    emailText = document.getElementById("feedbackEmailId").value;
+
+    let feedbackInstance = {
+      feedback: feedbackText,
+      email: emailText
+    };
+    return feedbackInstance;
+  }
+
+  function submitFeedback(localRetrievedFeedback){
+    feedbackInString = JSON.stringify(localRetrievedFeedback);
+    console.log(feedbackInString);
+    alert("Feedback submitted! Thank you!");
+    document.getElementById("feedbackTextId").value = "";
+    document.getElementById("feedbackEmailId").value = "";
+  }
+
+  function clickSubmitButton (){
+    submitFeedback(retrieveFeedback());
+  }
+
 });
